a005459ad6486a97659817996578805a7d6b755f
gildemberg-santos <gildemberg.santos@gmail.com>
2023-11-05 22:27:26 -0300

    Ajustar o nome com o sexo

--- test/factories/lead.rb
+++ test/factories/lead.rb
@@ -2,8 +2,13 @@ FactoryBot.define do
   factory :lead do
     association :company
 
-    name { Faker::Name.name }
+    sex { %w[male female].sample }
+    name { sex == "male" ? Faker::Name.masculine_name : Faker::Name.feminine_name }
     phone { Faker::PhoneNumber.cell_phone }
     email { Faker::Internet.email }
+    country { Faker::Address.country }
+    state { Faker::Address.state }
+    city { Faker::Address.city }
+    age { Faker::Number.between(from: 18, to: 65) }
   end
 end
